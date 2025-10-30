e032f56d62320b50c6e2613c442c40e603d371ea
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-10-30 13:45:03 +0000

    add new tagline

--- src/components/SiteHeader/SiteHeader.jsx
+++ src/components/SiteHeader/SiteHeader.jsx
@@ -1,7 +1,7 @@
 import styles from "./SiteHeader.module.css";
 
 function SiteHeader() {
-  return <div className={styles.wrapper}>SiteHeader</div>;
+  return <div className={styles.wrapper}>Don't React, Stay Calm</div>;
 }
 
 export default SiteHeader;
