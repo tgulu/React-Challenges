32778fa13009242510a5688aef6ff2e0c5ba0a19
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-10-30 14:14:38 +0000

    add new color scheme for page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,7 +3,9 @@ import styles from "./Home.module.css";
 function Home() {
   return (
     <div className={styles.wrapper}>
-      <p>Home</p>
+      <div className={styles.container}>
+        <h1>Home</h1>
+      </div>
     </div>
   );
 }
