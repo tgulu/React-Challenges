ec19d5a1f2fc57724f957760c46f2ea962981201
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-10-30 13:45:28 +0000

    make new useState page

---
+++ src/components/UseState/UseState.jsx
@@ -0,0 +1,15 @@
+function useState() {
+  return (
+    <div>
+      <div className="container">
+        <h1>useEffect Component</h1>
+        <div className="card">
+          <button>Click Me</button>
+          <p>Button clicked 0 times</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default useState;
