c0b74b6cc8083848f67aaddb3c002bf1f8fd8c1b
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-10-30 16:06:13 +0000

    add logic for password

--- src/components/UseState/UseState.jsx
+++ src/components/UseState/UseState.jsx
@@ -1,15 +1,43 @@
-function useState() {
+import { useState } from "react";
+import styles from "./UseState.module.css";
+
+function UseState() {
+  const password = "swordfish";
+  const [authorized, setAuthorized] = useState(false);
+
+  function handleSubmit(e) {
+    e.preventDefault(); // prevents form refresh
+    const enteredPassword = e.target.querySelector(
+      'input[type="password"]'
+    ).value;
+    const auth = enteredPassword === password;
+    setAuthorized(auth);
+  }
+
+  const login = (
+    <form onSubmit={handleSubmit}>
+      <input type="password" placeholder="Password" />
+      <input type="submit" value="Submit" />
+    </form>
+  );
+
+  const contactInfo = (
+    <ul>
+      <li>Micheal@Jordan.com</li>
+      <li>999.666.999</li>
+    </ul>
+  );
+
   return (
-    <div>
-      <div className="container">
-        <h1>useEffect Component</h1>
-        <div className="card">
-          <button>Click Me</button>
-          <p>Button clicked 0 times</p>
+    <div className={styles.wrapper}>
+      <div className={styles.container}>
+        <div className={styles.card}>
+          <h1>{authorized ? "Contact" : "Enter the Password"}</h1>
+          {authorized ? contactInfo : login}
         </div>
       </div>
     </div>
   );
 }
 
-export default useState;
+export default UseState;
